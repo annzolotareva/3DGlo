0558dd61579614356341d9eddeda1d71f3f786d1
Anna Zolotareva <anazolotarewa@yandex.ru>
2022-01-02 02:19:25 +0400

    создала модуль для калькулятора. В калькуляторе разрешила только ввод цифр

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import timer from "./modules/timer.js";
 import menu from "./modules/menu.js";
 import modal from "./modules/modal.js";
+import calculator from "./modules/calculator.js";
 
 timer("30 december 2021");
 menu();
 modal();
+calculator();
