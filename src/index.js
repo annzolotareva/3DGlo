9bc8729a9f11b54ff2ee588e951fe7d7500f1ba3
Anna Zolotareva <anazolotarewa@yandex.ru>
2022-01-03 23:26:01 +0400

    написала функцию, которая добавляет на страницу точки с классом dot

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import modal from "./modules/modal.js";
 import calculator from "./modules/calculator.js";
 import validation from "./modules/validation.js";
 import tabs from "./modules/tabs.js";
+import slider from "./modules/slider.js";
 
 timer("30 december 2021");
 menu();
@@ -11,3 +12,4 @@ modal();
 calculator();
 validation();
 tabs();
+slider();
