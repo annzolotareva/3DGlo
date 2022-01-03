6d294747ea37034803dbffd877f4c77874c4c7f6
Anna Zolotareva <anazolotarewa@yandex.ru>
2022-01-04 02:53:52 +0400

    Реализован калькулятор

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import timer from "./modules/timer.js";
 import menu from "./modules/menu.js";
 import modal from "./modules/modal.js";
-import calculator from "./modules/calculator.js";
+import calc from "./modules/calc.js";
 import validation from "./modules/validation.js";
 import tabs from "./modules/tabs.js";
 import slider from "./modules/slider.js";
@@ -9,7 +9,7 @@ import slider from "./modules/slider.js";
 timer("30 december 2021");
 menu();
 modal();
-calculator();
+calc(100);
 validation();
 tabs();
 slider();
