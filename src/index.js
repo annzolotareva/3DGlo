cf02ca3802343d79c65d2c2be42e78289cc4e05a
Anna Zolotareva <anazolotarewa@yandex.ru>
2022-01-07 19:05:38 +0400

    Сделала валидацию данных при отправке

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import calc from "./modules/calc.js";
 import validation from "./modules/validation.js";
 import tabs from "./modules/tabs.js";
 import slider from "./modules/slider.js";
+import sendForm from "./modules/sendForm.js";
 
 timer("30 december 2021");
 menu();
@@ -13,3 +14,30 @@ calc(100);
 validation();
 tabs();
 slider();
+sendForm({
+  formId: "form1",
+  someElem: [
+    {
+      type: "block",
+      id: "total",
+    },
+  ],
+});
+sendForm({
+  formId: "form3",
+  someElem: [
+    {
+      type: "block",
+      id: "total",
+    },
+  ],
+});
+sendForm({
+  formId: "form2",
+  someElem: [
+    {
+      type: "block",
+      id: "total",
+    },
+  ],
+});
--- src/modules/validation.js
+++ src/modules/validation.js
@@ -1,48 +1,49 @@
 const validation = () => {
-  const inputsFirstScreen = document.querySelectorAll(".row");
+  // const forms = document.querySelectorAll(".user_form");
+  // const firstScreen = document.getElementById("form1");
+  // const formsBtns = document.querySelectorAll('form-btn[type="submit"]');
   // const inputsText = document.querySelectorAll('input[type="text"]');
   // const inputPlaceholder = document.querySelector('input[placeholder="Ваше сообщение"]');
   // const inputsEmail = document.querySelectorAll('input[type="email"]');
   // const inputsTel = document.querySelectorAll('input[type="tel]');
-  // const forms = document.querySelectorAll("form");
-
-  // forms.forEach((form) => {
-  //   form.addEventListener("submit", (e) => {
-  //     e.preventDefault();
-
-  //     inputsText.forEach((input) => {
-  //       if (/^[а-я\-\s]/gi.test(input.value)) {
-  //         alert("В инпуте должна быть только кириллица, дефис или пробел!");
-  //       }
-  //       if (input.value == " ") {
-  //         alert("Введите значения");
-  //       }
-  //     });
-
-  //     if (/^[а-я\-\s]/gi.test(inputPlaceholder.value)) {
-  //       alert("В инпуте должна быть только кириллица, дефис или пробел!");
-  //     }
-
-  //     if (inputPlaceholder.value == " ") {
-  //       alert("Введите значения");
-  //     }
-
-  //     inputsEmail.forEach((input) => {
-  //       if (/^[\w\@\-\.\!\~\*\']/gi.test(input.value)) {
-  //         alert("В инпуте должна быть только латиница в любом регистре, цифры и спецсимволы:  @  -  _  . ! ~ * '");
-  //       }
-  //       if (input.value == " ") {
-  //         alert("Введите значения");
-  //       }
-  //     });
-
-  //     inputsTel.forEach((input) => {
-  //       if (/^[\d\(\)\-]/gi.test(input.value)) {
-  //         alert("В инпуте должны быть только цифры, круглые скобки и дефис");
-  //       }
-  //       if (input.value == " ") {
-  //         alert("Введите значения");
-  //       }
+  // const inputTextValidation = (input) => {
+  //   if (/^[а-я\-\s]/gi.test(input.value)) {
+  //     alert("В инпуте должна быть только кириллица, дефис или пробел!");
+  //   }
+  //   if (input.value == " ") {
+  //     alert("Введите значения");
+  //   }
+  // };
+  // const inputsEmailValidation = (input) => {
+  //   if (/^[\w\@\-\.\!\~\*\']/gi.test(input.value)) {
+  //     alert("В инпуте должна быть только латиница в любом регистре, цифры и спецсимволы:  @  -  _  . ! ~ * '");
+  //   }
+  //   if (input.value == " ") {
+  //     alert("Введите значения");
+  //   }
+  // };
+  // const inputinputsTelValidation = (input) => {
+  //   if (/^[\d\(\)\-]/gi.test(input.value)) {
+  //     alert("В инпуте должны быть только цифры, круглые скобки и дефис");
+  //   }
+  //   if (input.value == " ") {
+  //     alert("Введите значения");
+  //   }
+  // };
+  // formsBtns.forEach((btn) => {
+  //   firstScreen.addEventListener("submit", () => {
+  //     btn.addEventListener("click", (e) => {
+  //       e.preventDefault();
+  //       inputsText.forEach((input) => {
+  //         inputTextValidation(input);
+  //       });
+  //       inputTextValidation(inputPlaceholder);
+  //       inputsEmail.forEach((input) => {
+  //         inputsEmailValidation(input);
+  //       });
+  //       inputsTel.forEach((input) => {
+  //         inputinputsTelValidation(input);
+  //       });
   //     });
   //   });
   // });
